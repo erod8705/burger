56d224e379e9e7a0258dacbf1c00aba7d0c7df52
Erica Rodriguez <erica.rodriguez.mail@gmail.com>
2017-06-12 00:08:02 -0500

    added burger.html files etc.

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('Cowboy Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Avacado Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Pepper Jack Burger', true);
+INSERT INTO burger (burger_name, devoured) VALUES ('Bacon Burger', false);
